f64240e242fa013b7dbf26e84694a0e9a9c60cb4
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-11-09 01:00:17 +0530

    v1

---
+++ apps/server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("NexBus Server is running");
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3000");
+});
+
