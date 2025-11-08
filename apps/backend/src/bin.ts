e615c41e924d0bdee426502814db4786f12663c9
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-11-09 00:06:35 +0530

    v1

--- apps/backend/src/bin.ts
+++ apps/backend/src/bin.ts
@@ -1,6 +1,9 @@
 import { app } from "./index";
 
 const PORT = process.env.PORT || 3001;
+app.get("/", (req, res) => {
+    res.send("NexBus Backend is running");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
