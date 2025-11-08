90bbf7c8071df2390de97ad6dc72dcd27265849f
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-11-09 00:31:27 +0530

    v1

--- apps/backend/dist/lib/__mocks__/client.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const jest_mock_extended_1 = require("jest-mock-extended");
-exports.prisma = (0, jest_mock_extended_1.mockDeep)();
--- apps/backend/dist/lib/client.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const prisma_1 = require("@repo/db/generated/prisma");
-exports.prisma = new prisma_1.PrismaClient();
